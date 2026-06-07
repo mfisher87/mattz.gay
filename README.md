a74fd717acabd7edb11b8bab7553e295356b928c
Matt Fisher <mfisher87@gmail.com>
2026-06-06 18:15:06 -0600

    Disable redirect in dev, add redirect override query param

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ The only exception is an HTTP server -- this is needed to use ES Modules.
 ## Development
 
 Use `python preview.py` or any other HTTP server to preview the page.
+
+
+### Redirection
+
+Redirection is annoying during development. So, when visiting this page deployed at
+`localhost`, redirection is automatically disabled.
+
+You can override the automatic behavior with the URL query parameter `?redirect` or
+`?noredirect`.
--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,22 @@
+const shouldRedirect = () => {
+  const params = new URLSearchParams(window.location.search);
+  if (params.has("redirect")) {
+    console.debug("Redirect forced by 'redirect' query param!");
+    return true;
+  }
+  if (params.has("noredirect")) {
+    console.debug("Redirect disabled by 'noredirect' query param!");
+    return false;
+  }
+
+  if (["localhost", "127.0.0.1"].includes(window.location.hostname)) {
+    console.debug("Redirect disabled for local deployment!");
+    return false;
+  }
+
+  return true;
+}
+
 const redirectTimer = ({delaySeconds, destination}) => {
   setTimeout(() => {
     window.location.href = destination;
